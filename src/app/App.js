aa62e05fbfefa77f0e96b10a784f181e065b8ed3
exosyphon <exosyphon@yahoo.com>
2018-03-03 14:46:43 -0600

    Ejected react-scripts

---
+++ src/app/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Warehouse Viewer</h1>
+        </header>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <td>Hi</td>
+                <td>Hi</td>
+                <td>Hi</td>
+                <td>Hi</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
